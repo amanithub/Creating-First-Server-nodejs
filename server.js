78aafdfcfb26d0d897472e9222de8d128990643c
Aman Pandey <pandeyamankumar13@gmail.com>
2022-08-26 15:52:22 +0530

    Completed my own code for first node.js sever

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = 8081;
+
+http
+   .createServer((require, response) => {
+  response.writeHead(200, {"content-Type": "text/html"});
+  response.write("<h1>Hello, this is from my server</h1>");
+  response.end();
+})
+.listen(port, () => {
+    console.log(`Nodejs server started on port ${port}`);
+});
+
+// http://Localhost:8081
+
